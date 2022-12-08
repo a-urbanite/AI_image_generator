679b69bcb786b2d5ff88b0596bd44b0884604a1e
a.urbanite <a.urbanite@gmail.com>
2022-12-08 22:56:57 +0100

    MVP working

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -8,33 +8,31 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const generateImage = async (req, res) => {
-
-  const {prompt, size, number} = req.body
-  const imageSize = size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024'
+  const { prompt, size, number } = req.body;
+  const imageSize = size === "small" ? "256x256" : size === "medium" ? "512x512" : "1024x1024";
 
   try {
     const response = await openai.createImage({
       prompt: prompt,
       n: 1,
-      size: imageSize
+      size: imageSize,
     });
-    console.log(response.data)
-    const imageUrl = response.data.data[0].url
+    const imageUrl = response.data.data[0].url;
     res.status(200).json({
       success: true,
-      data: imageUrl
-    })
+      data: imageUrl,
+    });
   } catch (error) {
     if (error.response) {
-      console.log(error.response.status)
-      console.log(error.response.data)
+      console.log(error.response.status);
+      console.log(error.response.data);
     } else {
-      console.log(error.message)
+      console.log(error.message);
     }
     res.status(400).json({
-      success:false,
-      error: "the image could not be generated"
-    })
+      success: false,
+      error: "the image could not be generated",
+    });
   }
 };
 
---
+++ public/js/main.js
@@ -0,0 +1,22 @@
+window.document.getElementById('button').addEventListener('click', async () => {  
+  const prompt = window.document.getElementById('promptInput').value
+  const imageSize = window.document.getElementById('sizeInput').value
+  // console.log(prompt, imageSize)
+
+  const response = await fetch('/openai/generateImage', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      prompt,
+      imageSize,
+    }),
+  });
+
+  const data = await response.json()
+
+  window.document.getElementById('image').setAttribute('src', data.data)
+
+  console.log(data)
+})
